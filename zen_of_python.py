5449ef65424116965f2e628f0a36f91cb044c6c4
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-04-06 21:21:17 +0530

    Added rulers to keep length of code in check

--- zen_of_python.py
+++ zen_of_python.py
@@ -1,2 +1,2 @@
 import this
-# Run this code to see the Zen of Python, which is a collection of guiding principles for writing computer programs in Python.
+# Run this code to see the Zen of Python.
