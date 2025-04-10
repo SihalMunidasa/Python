27d8dee264757d716e608b820e717089ed66129e
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-04-11 00:37:14 +0530

    Set the game frame rate

--- .vscode/alien-invasion/alien_invasion.py
+++ .vscode/alien-invasion/alien_invasion.py
@@ -11,6 +11,7 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.clock = pygame.time.Clock()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -22,6 +23,9 @@ class AlienInvasion:
             
             # Make the most recently drawn screen visible
             pygame.display.flip()
+
+            # Set game frame rate to 60fps
+            self.clock.tick(60)
 
 if __name__ == "__main__":
     # Make a game instance, and run the game.
