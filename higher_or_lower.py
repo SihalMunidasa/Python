e0a1efcee966d7ec3ec50dd3bd9723caf039d9f6
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-04-15 03:27:33 +0530

    Added some additional  comments

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -2,13 +2,13 @@
 import random
 
 # setting the 2 numbers
-num_1 = random.randint(1, 100)
-num_2 = random.randint(1, 100)
+num_1 = random.randint(1, 10)
+num_2 = random.randint(1, 10)
 
 # making sure both numbers are not equal
 def num_not_equal(num_1, num_2):
     while num_1 == num_2:
-        num_2 = random.randint(1, 100)
+        num_2 = random.randint(1, 10)
     return num_2
 
 # Making sure both numbers are not equal before the start of the game
@@ -25,11 +25,13 @@ while game:
     guess = input("Higher or Lower? ")
 
     # The Reveal
+    # If user guesses correct, old number is stored and changed for a new guess
     if (num_2 > num_1 and guess == "Higher") or (num_2 < num_1 and guess == "Lower"):
         print("Correct\n")
         num_1 = num_2
         num_2 = num_not_equal(num_1, num_2)
 
+    # If the user is wrong, the game ends
     else:
         print("Wrong\n")
         game = False
