435f59d90f30f673bb8e484dcc2ae3807dca5c44
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-04-14 12:32:51 +0530

    Higher or Lower

---
+++ higher_or_lower.py
@@ -0,0 +1,12 @@
+import random
+
+answer = random.randint(1, 100)
+guess = int(input())
+
+while guess != answer:
+    if guess > answer:
+        print("Lower")
+    elif guess < answer:
+        print("Higher")
+
+print("Correct")
