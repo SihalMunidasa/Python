7e66fde689466c8b445672fe998e28691fa84dea
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-04-15 03:21:09 +0530

    Basic logic of Higher or Lower game

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,13 +1,35 @@
+# Importing the random module for the random numbers
 import random
 
-answer = random.randint(1, 100)
-guess = int(input())
+# setting the 2 numbers
+num_1 = random.randint(1, 100)
+num_2 = random.randint(1, 100)
 
-while guess != answer:
-    if guess > answer:
-        print("Lower")
-    elif guess < answer:
-        print("Higher")
-    guess = int(input())
+# making sure both numbers are not equal
+def num_not_equal(num_1, num_2):
+    while num_1 == num_2:
+        num_2 = random.randint(1, 100)
+    return num_2
 
-print("Correct")
+# Making sure both numbers are not equal before the start of the game
+num_2 = num_not_equal(num_1, num_2)
+
+# setting guess to True, simulating an on/off switch for the game
+game = True
+
+while game:
+    # Display the first number
+    print(num_1)
+
+    # User's guess
+    guess = input("Higher or Lower? ")
+
+    # The Reveal
+    if (num_2 > num_1 and guess == "Higher") or (num_2 < num_1 and guess == "Lower"):
+        print("Correct\n")
+        num_1 = num_2
+        num_2 = num_not_equal(num_1, num_2)
+
+    else:
+        print("Wrong\n")
+        game = False
